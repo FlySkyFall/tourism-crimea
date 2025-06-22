53a31989a91cfa217772979821ab134579e5a3a3
FlySkyFall <flyskyfall@mail.ru>
2025-06-22 11:56:37 +0300

    fixed uri

--- routes/attractionRoutes.js
+++ routes/attractionRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const attractionController = require('../controllers/AttractionController');
+const attractionController = require('../controllers/attractionController');
 
 router.get('/', attractionController.getAttractions);
 router.get('/:id', attractionController.getAttractionById);
